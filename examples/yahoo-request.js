a0d1e10e48dcf4c4d2c3d1c765131d93b2bf2eb2
Nathan Rajlich <nathan@tootallnate.net>
2010-11-14 12:12:22 -0800

    More progress. 'request' and 'response' events now actually get fired.

---
+++ examples/yahoo-request.js
@@ -0,0 +1,36 @@
+var HttpRequestStack = require('../lib/http-stack').HttpRequestStack;
+
+// Create the 'low-level' net.Stream we're going to be 'stacking' on
+var conn = require('net').createConnection(80, 'www.yahoo.com');
+
+conn.on('connect', function() {
+  
+  // Create our first "stack", the HttpRequestStack instance. This class is
+  // responsible for writing an HTTP request to the provided 'conn', and then
+  // parsing the response into a 'response' event and clean 'data' events.
+  var req = new HttpRequestStack(conn);
+  
+  req.get("/", [
+    "Host: www.yahoo.com",
+    "Connection: close",
+    "Accept-Encoding: gzip"
+  ]);
+  req.end();
+  
+  // 'response' is fired after the final HTTP header has been parsed. 'res'
+  // is a ReadStream, that also contains 'rawHeaders', 'headers' properties.
+  req.on('response', function(res) {
+
+    res.pipe(process.stdout);
+  
+    res.on('data', function(chunk) {
+      //console.error(chunk.toString());
+    });
+  
+    res.on('end', function() {
+      //console.error('received FIN packet from "conn"');
+    });
+    
+  });
+  
+});
--- tests/test-request-simple-curl-get.js
+++ tests/test-request-simple-curl-get.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var HttpResponseStack = require('../lib/http-stack').HttpResponseStack;
 
 
-var rs = fs.createReadStream(__dirname + '/dumps/requests/simple-curl-get.dump');
+var rs = fs.createReadStream(__dirname + '/dumps/requests/simple-curl-get.dump', {bufferSize:1});
 
 var res = new HttpResponseStack(rs);
 
