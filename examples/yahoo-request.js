f5a87f2e5338edbf3b4104acf7951062d1ddb694
Nathan Rajlich <nathan@tootallnate.net>
2011-01-21 18:39:26 -0800

    chunked-encoding

--- examples/yahoo-request.js
+++ examples/yahoo-request.js
@@ -5,17 +5,18 @@ var conn = require('net').createConnection(80, 'www.yahoo.com');
 
 conn.on('connect', function() {
   
+  console.log('connected!');
   // Create our first "stack", the HttpRequestStack instance. This class is
   // responsible for writing an HTTP request to the provided 'conn', and then
   // parsing the response into a 'response' event and clean 'data' events.
   var req = new HttpRequestStack(conn);
   
   req.get("/", [
-    "Host: www.yahoo.com",
-    "Connection: close",
-    "Accept-Encoding: gzip"
+    "Host: www.yahoo.com"
+    //"Connection: close",
+    //"Accept-Encoding: gzip"
   ]);
-  req.end();
+  //req.end();
   
   // 'response' is fired after the final HTTP header has been parsed. 'res'
   // is a ReadStream, that also contains 'rawHeaders', 'headers' properties.
@@ -28,7 +29,7 @@ conn.on('connect', function() {
     });
   
     res.on('end', function() {
-      //console.error('received FIN packet from "conn"');
+      console.error('received FIN packet from "conn"');
     });
     
   });
