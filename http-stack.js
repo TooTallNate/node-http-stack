af9bd499be6ae362c5e2b5d8eedb745d901f0de3
Nathan Rajlich <nathan@tootallnate.net>
2011-04-05 13:24:19 -0700

    Removed all header parsing logic and replaced it with `node-header-stack`.

--- http-stack.js
+++ http-stack.js
@@ -1,9 +1,8 @@
-var inherits    = require('util').inherits;
-var StreamStack = require('stream-stack').StreamStack;
-var utils = require('./utils');
+var inherits     = require('util').inherits;
+var StreamStack  = require('stream-stack').StreamStack;
+var Headers      = require('header-stack').Headers;
+var HeaderParser = require('header-stack').Parser;
 
-var CRLF = '\r\n';
-var END_OF_HEADER = new Buffer(CRLF + CRLF);
 var STATUS_CODES = require('http').STATUS_CODES;
 
 /**
@@ -15,16 +14,16 @@ function HttpBaseStack(stream) {
   StreamStack.call(this, stream, {
     data: this._onData
   });
+  this._headerParser = new HeaderParser({
+    emitFirstLine: true
+  });
+  this._headerParser.on('firstLine', this._onFirstLine.bind(this));
 }
 inherits(HttpBaseStack, StreamStack);
 exports.HttpBaseStack = HttpBaseStack;
 
 // The HTTP version string to use for this request or response.
 HttpBaseStack.prototype.httpVersion = 'HTTP/1.1';
-
-// Flag indicating if this StreamStack is currently parsing the HTTP header,
-// or the content body.
-HttpBaseStack.prototype.parsingHeader = true;
 
 // Calling 'write()' transparently adds the HTTP "Transfer-Encoding".
 HttpBaseStack.prototype.write = function(chunk, enc) {
@@ -58,28 +57,9 @@ HttpBaseStack.prototype._writeChunk = function(chunk, enc) {
 }
 
 HttpBaseStack.prototype._writeHeader = function(firstLine, headers) {
-  // Allow a regular Object to be passed, and coerce it into an Array before processing
-  if (!Array.isArray(headers)) {
-    var h = [];
-    for (var i in headers) {
-      h.push(i + ': ' + headers[i]);
-    }
-    headers = h;
-  }
-  var req = firstLine + CRLF;
-  var cur;
-  for (var i=0, l=headers.length; i<l; i++) {
-    cur = headers[i];
-    if (cur.name && cur.value) {
-      // If the item is an Object with a 'name' and 'value', then use that
-      req += cur.name + ': ' + cur.value + CRLF;
-    } else {
-      // Otherwise it should be a full header line String
-      req += cur + CRLF;
-    }
-  }
-  //console.error('HTTP Request Header:\n' + req);
-  return this.stream.write(req + CRLF);
+  return this.stream.write(Headers(headers).toString({
+    firstLine: firstLine
+  }));
 }
 
 // For both a request and response, when data is recieved, the first thing
@@ -88,61 +68,29 @@ HttpBaseStack.prototype._writeHeader = function(firstLine, headers) {
 // the header. Once the header has been recieved, the content body will be
 // reemitted on the child stream.
 HttpBaseStack.prototype._onData = function(data) {
-  if (this.parsingHeader) {
-    if (!this.rawHeaders) {
-      this.rawHeaders = data;
-    } else {
-      this.rawHeaders = utils.bufferConcat(this.rawHeaders, data);
-    }
-    var index = utils.bufferIndexOf(this.rawHeaders, END_OF_HEADER);
-    //console.error(index, this.rawHeaders.length);
-    if (index > 0) {
-      var leftover;
-      var end = index+END_OF_HEADER.length;
-      if (end < this.rawHeaders.length) {
-        leftover = this.rawHeaders.slice(end);
-        this.rawHeaders = this.rawHeaders.slice(0, end);
-      }
-      
-      this._onHeadersComplete();
-      //console.error(this.rawHeaders);
-      //console.error(leftover);
-      
-      this.parsingHeader = false;
-      if (leftover) this._onData(leftover);
-    }
+  if (this._headerParser) {
+    this._headerParser.parse(data);
   } else {
     this.emit('data', data);
   }
 }
 
-/**
- * This gets called only once; after the final (empty) header line has been
- * parsed. This function parses the raw header response into a 'headers'
- * object, and gets attached to a new StreamStack that gets returned with
- * the 'request' or 'response' event (depending of the type of instance this
- * is).
- */
-HttpBaseStack.prototype._onHeadersComplete = function() {
+// Fired from the 'headerParser' after the first line has been parsed.
+HttpBaseStack.prototype._onFirstLine = function(line) {
   var stream = new StreamStack(this);
-  var lines = this.rawHeaders.toString().split(CRLF);
-  this._onFirstLine(lines[0], stream);
-  stream.headers = lines.slice(1, lines.length-2);
-  stream.headers.forEach(function(line, i) {
-    var firstColon = line.indexOf(':');
-    var name = line.substring(0, firstColon);
-    var value = line.substring(firstColon+(line[firstColon+1] == ' ' ? 2 : 1));
-    // Individual header lines can be retreived directly as a full String on the
-    // array, or by accessing the 'name' and 'value' properties on each header line.
-    stream.headers[i] = new String(line);
-    stream.headers[i].name = name;
-    stream.headers[i].value = value;
-    // To match node's API; lowercase the header name as a prop on the array.
-    // This is more convenient, but won't work well with multiple headers of the
-    // same name (i.e. Set-Cookie).
-    stream.headers[name.toLowerCase()] = value;
-  });
-  stream.headers.inspect = utils.headerInspect;
+  this._headerParser.on('headers', function(headers, leftover) {
+    this._onHeaders(headers, leftover, stream);
+  }.bind(this));
+  this._parseFirstLine(line, stream);
+}
+
+// Fired from the 'headerParser' after the empty line (indicating the end of
+// the HTTP headers) has been fired. It attaches the given 'headers' object
+// to the 'stream' instance passed, and the 'request' or 'response' event
+// (depending of the type of instance this is) is fired.
+HttpBaseStack.prototype._onHeaders = function(headers, leftover, stream) {
+  this._headerParser = null;
+  stream.headers = headers;
   this.emit(this._headerCompleteEvent, stream);
 }
 
@@ -158,8 +106,8 @@ function HttpRequestStack(stream) {
 inherits(HttpRequestStack, HttpBaseStack);
 exports.HttpRequestStack = HttpRequestStack;
 
-// Bool to keep track of whether or not this 'HttpRequestStack' instance has
-// had a request sent on it already.
+// Bool to keep track of whether or not this 'HttpRequestStack'
+// instance has had a request sent on it already.
 HttpRequestStack.prototype.requestSent = false;
 
 HttpRequestStack.prototype.request = function(method, path, headers) {
@@ -184,7 +132,7 @@ HttpRequestStack.prototype._headerCompleteEvent = "response";
 
 // Parses the first HTTP header line into 'httpVersion', 'statusCode', and
 // 'statusMessage' properties.
-HttpRequestStack.prototype._onFirstLine = function(line, res) {
+HttpRequestStack.prototype._parseFirstLine = function(line, res) {
   var i = line.indexOf(' ');
   res.httpVersion = line.substring(0, i);
   var j = line.indexOf(' ', i+1);
@@ -220,7 +168,7 @@ HttpResponseStack.prototype.writeHead = function(statusCode, headers) {
 HttpResponseStack.prototype._headerCompleteEvent = "request";
 
 // Parses the first HTTP header line into 'method', 'path', and 'httpVersion' props.
-HttpResponseStack.prototype._onFirstLine = function(line, req) {
+HttpResponseStack.prototype._parseFirstLine = function(line, req) {
   var i = line.indexOf(' ');
   req.method = line.substring(0, i);
   var j = line.indexOf(' ', i+1);
